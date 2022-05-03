616d0ead5749408e23758e5c8f5c9ae4a1f2889f
nm <>
2022-05-03 14:37:04 +0900

    article#edit,delete作成 CRUD機能は一通り完成

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+	before_action :set_article, only: [:edit, :update]
+
 	# 投稿一覧画面
 	def index
 		@articles = Article.all
@@ -10,7 +12,7 @@ class ArticlesController < ApplicationController
 	end
 
 	def create
-		@article = Article.new(article_paramas)
+		@article = Article.new(article_params)
 		if @article.save
 			redirect_to root_path
 		else
@@ -18,9 +20,32 @@ class ArticlesController < ApplicationController
 		end
 	end
 
+	# 編集画面
+	def edit
+	end
+
+	def update
+		if @article.update(article_params)
+			redirect_to root_path
+		else 
+			render :edit
+		end
+	end
+
+	# 削除
+	def destroy
+		article = Article.find(params[:id])
+		article.destroy!
+		redirect_to root_path
+	end
+
 	private
-	def article_paramas
+	def article_params
 		params.require(:article).permit(:content)
+	end
+
+	def set_article
+		@article = Article.find(params[:id])
 	end
 
 end
